c34b0cd165b2307560184984f480ea0fd47c48b1
Greenhouse89 <greenbrandon07@yahoo.com>
2021-07-15 17:12:25 -0400

    creating search bar

--- video-browser/src/main.js
+++ video-browser/src/main.js
@@ -1,1 +1,12 @@
 import Vue from 'vue';
+import App from './App';
+
+new Vue ({
+    //el: '#app', is getting replaced by .$mount below
+    render: h => h(App)
+    /* function(h) {
+        return h(App);
+        h is create element  */
+         
+    
+}).$mount('#app');
