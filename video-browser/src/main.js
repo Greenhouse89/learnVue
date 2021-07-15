91754cdd10aaf870c1edc6a1be3446305bffbbba
Greenhouse89 <greenbrandon07@yahoo.com>
2021-07-15 17:27:49 -0400

    created onInput method for search bar

--- video-browser/src/main.js
+++ video-browser/src/main.js
@@ -2,11 +2,9 @@ import Vue from 'vue';
 import App from './App';
 
 new Vue ({
-    //el: '#app', is getting replaced by .$mount below
+    el: '#app', //is getting replaced by .$mount below
     render: h => h(App)
     /* function(h) {
         return h(App);
         h is create element  */
-         
-    
-}).$mount('#app');
+})//.$mount('#app');
