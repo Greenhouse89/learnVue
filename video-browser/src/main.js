eb9100510ccff7f1f0ae6128f94c2898bdccc523
Greenhouse89 <greenbrandon07@yahoo.com>
2021-07-17 08:32:27 -0400

    add styling to video list item component

--- video-browser/src/main.js
+++ video-browser/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App';
 
 new Vue ({
-    el: '#qpp', //is getting replaced by .$mount below
+    el: '#app', //is getting replaced by .$mount below
     render: h => h(App)
     /* function(h) {
         return h(App);
